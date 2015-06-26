f664632b5fd9a72cdf6408272b1b662f0f8c2a24
lesell_b <lesell_b@epitech.eu>
2015-06-26 04:26:39 +0200

    add working eventemitter system

--- eventemitter.lua
+++ eventemitter.lua
@@ -1,5 +1,41 @@
 --[[ EventEmitter ]]--
 
-local object = require("gears.object")
+local eventemitter = { mt = {} }
 
+local function on(self, event, func)
+	if not self._events[event] then
+		self._events[event] = {}
+	end
+	self._events[event][func] = func
+end
 
+local function emit(self, event, ...)
+	if not self._events[event] then
+		return
+	end
+	for func in pairs(self._events[event]) do
+        func(self, ...)
+    end
+end
+
+function eventemitter:new(obj)
+	local newobj = obj or {}
+	newobj._events = {}
+
+	function newobj:on(event, func)
+		on(self, event, func)
+	end
+
+	function newobj:emit(event, ...)
+		emit(self, event, ...)
+	end
+
+	return newobj
+end
+
+function eventemitter.mt:__call(...)
+	return eventemitter:new(...)
+end
+
+return setmetatable(eventemitter, eventemitter.mt)
+--return setmetatable({}, { __call = function(_, ...) return eventemitter:new(...) end })
