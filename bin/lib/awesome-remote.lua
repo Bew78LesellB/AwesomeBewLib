dc27ce3b904a03a3e97f4aa213ed7a86cdeb2fcf
lesell_b <lesell_b@epitech.eu>
2016-01-26 00:13:50 +0100

    awesome-remote-client is now working !!! usage: ./awesome-remote-client my::event "{ arg1 = 'value'}"

--- bin/lib/awesome-remote.lua
+++ bin/lib/awesome-remote.lua
@@ -1,11 +1,20 @@
 -- Module environment
 local AwesomeRemote = {}
 
+-- Grab environment
+local capi = {
+	print = print,
+}
+
 -- Grab dependancies
 local Socket = require("socket")
 local MsgPack = require("MessagePack")
 
 -- Private variables
+
+local config = {
+	verbose = false,
+}
 
 local host = "localhost"
 local remotePortDir = "/tmp/awesome-remote"
@@ -14,14 +23,23 @@ local servers = {}
 
 -- Private functions
 
+local function print(...)
+	if not config.verbose then
+		return false
+	end
+	return capi.print(...)
+end
+
 local function extractServerInfo(fileName)
 	if not fileName then return end
 
 	local file = io.open(fileName, "r")
 	if not file then return end
 
-	local infos = {}
-	-- no infos to extract right now...
+	local infos = {
+		port = file,
+	}
+	-- no more info to extract from file right now...
 	file:close()
 
 	return infos
@@ -38,13 +56,16 @@ end
 local function findRunningServers()
 	local files = lsFiles(remotePortDir)
 
+	print("finding running servers")
+
 	-- empty the list of servers
 	servers = {}
 
-	for _, port in ipairs(files) do
+	for _, file in ipairs(files) do
+		print("found " .. file)
 		table.insert(servers, {
-			port = port,
-			info = extractServerInfo(port),
+			port = file,
+			info = extractServerInfo(file),
 		})
 	end
 end
@@ -59,21 +80,36 @@ local function sendToServer(serverInfo, data)
 	local packet = MsgPack.pack(data)
 	local sent, status = tcp:send(string.len(packet) .. "\n")
 	local sent, status = tcp:send(packet)
+	-- TODO: do something with sent & status
 end
 
 local function sendToAllServers(data)
 	for _, serverInfo in ipairs(servers) do
-		sendToServer(serverInfo, data)
+		if not sendToServer(serverInfo, data) then
+			return false
+		end
+		return true
 	end
 end
 
 -- Public functions
 
-function AwesomeRemote.init()
-
+function AwesomeRemote.init(conf)
+	local conf = conf or {}
+	config.verbose = conf.verbose or false
+	findRunningServers()
 end
 
-function AwesomeRemote.sendEvent(name, data)
+function AwesomeRemote.sendEvent(eventName, eventArgs)
+	local data = {
+		format = "awesome",
+		type = "event",
+		data = {
+			name = eventName,
+			args = eventArgs,
+		}
+	}
+	return sendToAllServers(data)
 end
 
 return AwesomeRemote
