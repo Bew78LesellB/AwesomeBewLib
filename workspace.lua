8e11d9a86aeff32807a6c2757acd80a5d6754562
lesell_b <lesell_b@epitech.eu>
2016-06-02 01:28:47 +0200

    simplify workspace (still no code ^^)

--- workspace.lua
+++ workspace.lua
@@ -6,31 +6,9 @@ local Eventemitter = require("bewlib.eventemitter")
 local debug = require("gears.debug").dump_return
 
 -- Module environement
-local Workspace = {
-	mt = {},
-	prototype = {
-		mt = {},
-	},
-	_list = {},
-}
-Workspace = Eventemitter(Workspace)
+local Workspace = Eventemitter({})
 
-local prototype = Eventemitter({})
+Workspace.prototype = Eventemitter({})
 
-
-function Workspace.mt:__call(...)
-	--Workspace.new(...)
-end
-
-Workspace = setmetatable(Workspace, Workspace.mt)
-
---example/ideas
-local ws = Workspace.add({ -- add ? new ?
-	name = "Default",
-	screen = 1,
-	position = "left", --( possible : "right" "begin" "end" "2" "42"(end) )
-	relative = "end", -- position relative par rapport au dernier (la position sera donc : avant dernier)
-})
---end example/ideas
 
 return Workspace
