bff7dd3fcdf8a7223dd769eb4c7b952daa2e9872
Benoit de Chezelles <lesell_b@epitech.eu>
2018-07-23 04:30:29 -0700

    WIP (not tested) Add initial implem for ActionMenu

---
+++ action_menu.lua
@@ -0,0 +1,95 @@
+local wibox = require("wibox")
+local awful = require("awful")
+
+local Array = require("bewlib.std.array")
+
+------------------------------------------
+
+local ActionMenu = { prototype = {}, }
+
+function ActionMenu.new()
+  local instance = {
+    __class = ActionMenu,
+    _actions = Array.new(),
+    _header_text = nil,
+    _footer_text = nil,
+    _title = nil,
+    _wibox = nil,
+  }
+
+  return setmetatable(instance, { __index = ActionMenu.prototype })
+end
+
+function ActionMenu.prototype:add_action(key, text, callback)
+  local action = {
+    key = key,
+    text = text,
+    callback = callback,
+  }
+
+  self._actions:append(action)
+end
+
+function ActionMenu.prototype:set_header(header)
+  self._header_text = header
+end
+
+function ActionMenu.prototype:set_footer(footer)
+  self._footer_text = footer
+end
+
+function ActionMenu.prototype:set_title(title)
+  self._title = title
+end
+
+function ActionMenu.prototype:_build_menu()
+  local actions_setup = Array.from_table {
+    layout = wibox.layout.fixed.vertical,
+  }
+  self._actions.each(function(act)
+    actions_setup:append({
+      {
+        text = act.key,
+        font = "Awesome 20", -- TODO: tweak size!
+        widget = wibox.widget.textbox,
+
+        width = 10, -- TODO: tweak size!
+      },
+      {
+        text = act.text,
+        widget = wibox.widget.textbox,
+      },
+      layout = wibox.layout.fixed.horizontal,
+    })
+  end)
+
+  local actions_list_wibox = wibox.widget(actions_setup:to_table())
+  local w
+
+  -- Add title, header & footer
+
+  w = actions_list_wibox -- tmp
+  return w
+end
+
+function ActionMenu.prototype:show(config)
+  config = config or {}
+  local placement = config.placement or awful.placement.centered
+
+  if not self._wibox then
+    self._wibox = self:_build_menu()
+  end
+  placement(self._wibox)
+  self._wibox.visible = true
+
+  -- TODO: Start keyboard grabber...
+end
+
+function ActionMenu.prototype:hide()
+  if not self._wibox then
+    return
+  end
+  self._wibox.visible = false
+end
+
+return ActionMenu
